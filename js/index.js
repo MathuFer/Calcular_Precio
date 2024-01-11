3faed535815a011ee61d8cee6f8ee667f97c2636
MathuFer <103338842+MathuFer@users.noreply.github.com>
2024-01-11 20:24:20 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,3 @@
-// var cantidad = 0;
-
-// function sumar(){
-//     cantidad += 1;
-//     actualizar();
-//     calculartotal();
-// }
-
-// function restar(){
-//     cantidad -= 1;
-//     actualizar();
-//     calculartotal();
-// }
-
-// function actualizar() {
-//     document.getElementById("cantidad").textContent = cantidad;
-// }
-
-// var precio = 400000
-// precioSpan = document.querySelector(".precio-inicial");
-// precioSpan.innerHTML = formatNumber(precio)
-
-// function calculartotal(){
-//     var total = cantidad * precio;
-//     document.getElementById("valor-total").textContent = formatNumber(total);
-// }
-
-// function formatNumber(number) {
-//     return new Intl.NumberFormat('es-CL').format(number);
-// }
-
 var precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = formatNumber(precio)
